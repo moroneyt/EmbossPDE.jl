d4d836c42aad760dd062a9982a71dbf94471662e
Tim Moroney <t.moroney@qut.edu.au>
2024-10-09 14:23:02 +1000

    Update longbullet.jl

--- examples/Laplace/longbullet.jl
+++ examples/Laplace/longbullet.jl
@@ -3,27 +3,27 @@ using CairoMakie
 
 n = 60
 
-∂x, ∂y, B = operators(n)
+∂x, ∂y, B = operators([-2..2, -1..1], n)
 x,y = variables()
 
-# Domain (scaled from -2 ≤ 𝑥 ≤ 2, -1 ≤ 𝑦 ≤ 1)
-bottom = y =>-1, -1..1/2
-top    = y => 1, -1..1/2
-left   = x =>-1
-right  = x => y->1/2*(1+sqrt(1-y^2))
+# Domain
+bottom = y =>-1, -2..1
+top    = y => 1, -2..1
+left   = x =>-2
+right  = x => y-> 1 + sqrt(1-y^2)
 
-domain = (≥(bottom), ≤(top), ≥(left), ≤(right))
+domain = [≥(bottom), ≤(top), ≥(left), ≤(right)]
 
 # Equations
-pde = 1/4*∂x^2 + ∂y^2 => 0
-bcs = (B(bottom) => 2x, B(top) => 2x, B(left)*∂x => 0, B(right) => y->1+sqrt(1-y^2))
+pde = ∂x^2 + ∂y^2 => 0
+bcs = [B(bottom) => x->x, B(top) => x, B(left)*∂x => 0, B(right) => y->1+sqrt(1-y^2)]
 
 # Solution
 u = solve(pde, bcs...; domain)
-@show sample = u(0.5,-0.25) # arbitrary point
+@show sample = u(1,-0.25) # arbitrary point
 benchmark = 1.00625295597  # see MATLAB code below
 @show abs(sample-benchmark) / abs(benchmark)
-plot(u; aspect=2)
+plot(u)
 
 # Lightning Laplace solver comparision https://people.maths.ox.ac.uk/trefethen/lightning.html
 # u =  laplace({[1-1i 1] 1+1i -2+1i -2-1i}, {@(z) real(z), @(z) real(z), @(z) NaN*z, @(z) real(z)}, 'tol', 1e-10); u(1-0.25i)
