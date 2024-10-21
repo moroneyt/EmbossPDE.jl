1c657f1935a81a5c0fb2ea1b335743ffa9531257
Tim Moroney <t.moroney@qut.edu.au>
2024-10-21 15:39:25 +1000

    Update wavy_triangle.jl

--- examples/Laplace/wavy_triangle.jl
+++ examples/Laplace/wavy_triangle.jl
@@ -7,7 +7,7 @@ n = 60
 ∂x, ∂y, B, X, Y, Fx, Fy = operators([0..1, 0..1], n)
 x,y = variables()
 
-f(x) = x + 0.05*sinpi(4x)  # but not too wavy
+f(x) = x + 0.02*sinpi(4x)  # but not too wavy
 f′(x) = derivative(f,x)
 
 # Domain
@@ -23,4 +23,4 @@ bcs = (B(bottom) => x, B(right) => 1-y, B(hypot)*(Fx(f′)*∂x-∂y) => 0)
 
 # Solution
 u = solve(pde, bcs...; domain)
-plot(u)
+plot(u; divisions=512)
